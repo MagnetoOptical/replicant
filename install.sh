cbc9869f12d9d0f506b350112d7fe0c30b0d3548
Sean Seago <sean.seago@projectceladon.org>
2021-07-06 11:32:09 -0500

    Added license comments for clarity

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Copyright (C) 2021 Sean Seago
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 GPGKEY_LIST = ("https://download.docker.com/linux/debian/gpg")
 
 REPO_LIST = ("deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable")
